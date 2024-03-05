f648ea94b67ef933d3c4b78d8eda92f86b2b0580
Calvin Yuen <scyuenab@gmail.com>
2024-03-05 18:12:12 +0800

    feat: Add custom not-found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Link, redirect } from "@/lib/i18n";
 import Error from "next/error";
 
 // Render the default Next.js 404 page when a route
@@ -8,10 +10,18 @@ import Error from "next/error";
 
 export default function NotFound() {
   return (
-    <html lang="en">
-      <body>
-        <Error statusCode={404} />
-      </body>
-    </html>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col gap-4 rounded-md bg-white p-16">
+        <h1 className="text-4xl font-semibold">Page Not Found</h1>
+        <p className="text-xl">
+          Please click the button below to navigate to correct path
+        </p>
+        <Link href="/" locale="en">
+          <Button className="self-start text-xl" size="lg">
+            Back
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 }
